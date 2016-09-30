788e0eb5ec3fb6b45c57d6d413158363dd2bde41
Allendale Nato <natoallendale@gmail.com>
2016-09-30 19:44:40 +0800

    Added Schema.
    + Tweaked Routes.
    + Added login view.
    - Removed config/database.php in .gitignore

---
+++ FinDashboard/application/controllers/FinController.php
@@ -0,0 +1,31 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+/**
+ * The Main Controller of CSO Finance Dashboard.
+ *
+ * @author Gonzales, Eldes
+ * @author Nato, Allendale
+ * @author Sanchez, Mark
+ * @author Sotalbo, Faith
+ */
+class FinController extends CI_Controller{
+
+  /**
+   * Index Page for this Controller.
+   *
+   * If the URI contains no data, this function will be executed.
+   * The Controller will load the homepage if a session exists.
+   */
+  public function index(){
+    $this->load->view('login');
+  }
+
+  /**
+   * Loads the login page.
+   * @return [type] [description]
+   */
+  public function login(){
+
+  }
+
+}
--- FinDashboard/application/controllers/Welcome.php
+++ FinDashboard/application/controllers/Welcome.php
@@ -4,6 +4,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Welcome extends CI_Controller {
 
 	/**
+	 * Default CodeIgniter Controller.
+	 * 
 	 * Index Page for this controller.
 	 *
 	 * Maps to the following URL
