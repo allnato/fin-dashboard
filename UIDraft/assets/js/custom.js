251574456e8161c10ef2de7322c4b14ea6210f5c
Faith Clarisse Sotalbo <faith_sotalbo@dlsu.edu.ph>
2016-10-15 18:07:09 +0800

    Initial UI draft

---
+++ UIDraft/assets/js/custom.js
@@ -0,0 +1,25 @@
+function showContent(){
+        $(".activitycontent").slideToggle("slow", function(){
+            
+        });
+  /*  $(".activitycontent").slideToggle(500, "swing", function(){
+        $(".activitycontent").fadeIn("fast", function(){
+                $(".changeColor").css({"backgroundColor" : "ghostwhite"});
+        });
+    }
+        
+    });*/
+/*	$(".activitycontent").slideToggle(500, "swing", function(){
+		if ($(".activitycontent").is(":visible")){
+			$(".changeColor").fadeIn("fast", function(){
+				$(".changeColor").css({"backgroundColor" : "ghostwhite"});
+			});
+		}
+		else {
+			$(".changeColor").fadeIn("fast", function(){
+				$(".changeColor").css({"backgroundColor" : "white"});
+			});			
+		}
+	});
+*/
+}
