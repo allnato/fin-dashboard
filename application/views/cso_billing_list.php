5b55979d3701cfd10e51f0adb0acc64ed9ad1c93
Faith Clarisse Sotalbo <faithsotalbo@yahoo.com>
2016-12-15 03:03:40 +0800

    Fix title consistencies

--- application/views/cso_billing_list.php
+++ application/views/cso_billing_list.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 
-    <title>CSO | Activity List</title>
+    <title><?= $this->session->userdata('acronym')?> | Activity List</title>
 
     <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" name="viewport" />
     <meta name="viewport" content="width=device-width" />
--- application/views/cso_create_activity.php
+++ application/views/cso_create_activity.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 
-    <title>CSO | Create Activity</title>
+    <title><?= $this->session->userdata('acronym')?> | Create Activity</title>
 
     <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" name="viewport" />
     <meta name="viewport" content="width=device-width" />
